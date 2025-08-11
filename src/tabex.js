507cad0db2a1ccd6315940d383807bf1bcc4aa25
quoc-quy <quyleom10@gmail.com>
2025-08-11 15:42:50 +0700

    Tạo phương thức switch và destroy

--- src/tabex.js
+++ src/tabex.js
@@ -25,26 +25,26 @@ function Tabex(selector) {
         .filter(Boolean);
     if (this.tabs.length !== this.panels.length) return;
 
+    this._originalHTML = this.container.innerHTML;
+
     this._init();
 }
 
 Tabex.prototype._init = function () {
-    let tabActive = this.tabs[0];
-    tabActive.closest("li").classList.add("tabex--active");
-
-    this.panels.forEach((panel) => (panel.hidden = true));
+    this._activeTab(this.tabs[0]);
 
     this.tabs.forEach((tab) => {
         tab.onclick = (e) => this._handelTabClick(e, tab);
     });
-
-    const panelActive = this.panels[0];
-    panelActive.hidden = false;
 };
 
 Tabex.prototype._handelTabClick = function (e, tab) {
     e.preventDefault();
 
+    this._activeTab(tab);
+};
+
+Tabex.prototype._activeTab = function (tab) {
     this.tabs.forEach((tab) => {
         tab.closest("li").classList.remove("tabex--active");
     });
@@ -57,4 +57,39 @@ Tabex.prototype._handelTabClick = function (e, tab) {
 
     const panelActive = document.querySelector(tab.getAttribute("href"));
     panelActive.hidden = false;
+};
+
+Tabex.prototype.switch = function (input) {
+    let tabToActive = null;
+
+    if (typeof input === "string") {
+        tabToActive = this.tabs.find(
+            (tab) => tab.getAttribute("href") === input
+        );
+
+        if (!tabToActive) {
+            console.error(`Tabex: Not panel found with '${input}'`);
+            return;
+        }
+    } else if (this.tabs.includes(input)) {
+        tabToActive = input;
+    }
+
+    if (!tabToActive) {
+        console.error(`Tabex: Invalid input '${input}'`);
+        return;
+    }
+
+    this._activeTab(tabToActive);
+};
+
+Tabex.prototype.destroy = function () {
+    this.container.innerHTML = this._originalHTML;
+    this.panels.forEach((panel) => {
+        panel.hidden = false;
+    });
+
+    this.container = null;
+    this.tabs = null;
+    this.panels = null;
 };
